c50418451d3d5e51bec7268ef8a20b2c24c47ce8
JvovL <jjvovo715@gmail.com>
2023-12-05 18:49:04 +0300

    задание 3- 4

--- src/stack.js
+++ src/stack.js
@@ -12,19 +12,24 @@ const { NotImplementedError } = require("../extensions/index.js");
  * stack.pop(); // undefined
  *
  */
-module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+module.exports = class Stack
+ {
+  S = [];
+
+  push(element) 
+  {
+    this.S.unshift(element);
   }
 
-  pop() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  pop()
+   {
+    let top = this.S[0];
+    this.S.shift();
+    return top;
   }
 
-  peek() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  peek()
+   {
+    return this.S[0];
   }
 };
